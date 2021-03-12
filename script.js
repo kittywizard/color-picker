e4f1f3ef22f9d857727aafbd16eba12fdf510a97
marleigh <volcana@gmail.com>
2021-03-12 12:58:42 -0800

    no API needed! generating colors

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-let url = 'https://api.noopschallenge.com/hexbot?count=5';
 
-async function getColor(url) {
-    try {
-        let response = await fetch(url);
-        let json = await response.json();
-    
-        console.log(json);
-    }
-    catch(err) {
-        console.log(err);
+let colorNum = 16;
+const containerDiv = document.querySelector(".container");
+
+  //need to loop the function X number of times to get enough for a grid
+function displayColors() {
+    for(let i = 0; i < colorNum; i++) {
+        let result = randomHex();
+        let div = document.createElement("div");
+        div.textContent = result;
+        div.style.background = result;
+        div.classList.add("flex-div");
+        containerDiv.appendChild(div);
     }
 }
 
-getColor(url);
+displayColors();
+
+  //create a div square for each one 
+  //make the color the background
+  //add the hex code to the div
+  //flex!
+
+  //randomHex courtesy of : https://github.com/noops-challenge/hexbot#note-to-developers
+function randomHex() {
+    const hexMax = 256 * 256 * 256; //number of possibilities -  R G B?
+    return '#' + Math.floor(Math.random() * hexMax).toString(16).toUpperCase().padStart(6, '0');
+    //toString(16) returns a hexidecimal number
+    //padStart() 'pads' the number with 0s if it isn't 6 digits long
+  }
