4fdae0721216a7a7583d028bce04c1a0ed426175
marleigh <volcana@gmail.com>
2021-12-24 13:19:42 -0800

    surprisingly functional

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
-export default function App() {
-  return (
-    <div class="container">
+import React from 'react'
+import ColorBox from "./ColorBox";
+import randomHex from './randomHex';
 
-     <p>wow</p> 
-    </div>
+export default function App() {
+
+  let colorNum = 16; 
+  let colorArray = [];
+
+    //initially generates an array with 16 different colors
+    function generateArray(colorNum) {
+        //add things to object
+        for(let i = 0; i < colorNum; i++) {
+          const colorObj = {
+            id: i,
+            key: i,
+            hex: randomHex()
+          }
+          colorArray.push(colorObj);
+        }
+    }
+    generateArray(colorNum);
+
+    const [boxes, setBoxColor] = React.useState(colorArray);
+
+    //change the color of an individual box
+  function changeColor(id) {
+    setBoxColor(prevBox => {
+        return prevBox.map(box => {
+          return box.id === id ? {...prevBox, hex: randomHex()} : box 
+        })
+    })
+  }
+
+    const displayArr = boxes.map(color => (
+        <ColorBox 
+          key={color.key}
+          id={color.id}
+          hex={color.hex}
+          changeColor={() => changeColor(color.id)}
+          />
+      ))
+
+  return ( 
+      <div className= "container">
+          {displayArr}
+      </div>
   )
 }
